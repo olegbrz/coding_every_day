f60badcc58d630831ac19b81406b70ab45680305
Oleg <brezitskyyoleg@gmail.com>
2020-11-30 09:49:32 +0100

    minor refacor to follow the PEP convenions

--- 001_231120_pascals_triangle.py
+++ 001_231120_pascals_triangle.py
@@ -3,9 +3,12 @@ In mathematics, Pascal's triangle is a triangular array of the binomial
 coefficients expressed with formula (n k) = n!/(n-k)!, where n denotes a
 row of the triangle, and k is a position of a term in the row.
 
-Test.assert_equals(pascals_triangle(1), [1],"1 level triangle incorrect");
-Test.assert_equals(pascals_triangle(2), [1,1,1],"2 level triangle incorrect");
-Test.assert_equals(pascals_triangle(3), [1,1,1,1,2,1],"3 level triangle incorrect");
+Test.assert_equals(
+    pascals_triangle(1), [1],"1 level triangle incorrect");
+Test.assert_equals(
+    pascals_triangle(2), [1,1,1],"2 level triangle incorrect");
+Test.assert_equals(
+    pascals_triangle(3), [1,1,1,1,2,1],"3 level triangle incorrect");
 """
 
 import math
@@ -14,12 +17,12 @@ import math
 def pascals_triangle(n):
     triangle = []
 
-    def C(n, x):
+    def comb(n, x):
         return math.factorial(n)/(math.factorial(x)*math.factorial(n-x))
 
     for row in range(0, n):
         for col in range(0, row+1):
-            triangle.append(int(C(row, col)))
+            triangle.append(int(comb(row, col)))
 
     return triangle
 

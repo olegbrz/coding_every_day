6515b3773a8bcb8531b323d71e4385be86c4aea4
Oleg <brezitskyyoleg@gmail.com>
2020-12-17 23:51:11 +0100

    :christmas_tree: day 17

---
+++ 037_171220_AOC17.py
@@ -0,0 +1,484 @@
+"""--- Day 17: Conway Cubes ---
+As your flight slowly drifts through the sky, the Elves at the Mythical
+Information Bureau at the North Pole contact you. They'd like some help
+debugging a malfunctioning experimental energy source aboard one of their
+super-secret imaging satellites.
+
+The experimental energy source is based on cutting-edge technology: a set of
+Conway Cubes contained in a pocket dimension! When you hear it's having
+problems, you can't help but agree to take a look.
+
+The pocket dimension contains an infinite 3-dimensional grid. At every integer
+3-dimensional coordinate (x,y,z), there exists a single cube which is either
+active or inactive.
+
+In the initial state of the pocket dimension, almost all cubes start inactive.
+The only exception to this is a small flat region of cubes (your puzzle input);
+the cubes in this region start in the specified active (#) or inactive (.)
+state.
+
+The energy source then proceeds to boot up by executing six cycles.
+
+Each cube only ever considers its neighbors: any of the 26 other cubes where
+any of their coordinates differ by at most 1. For example, given the cube at
+x=1,y=2,z=3, its neighbors include the cube at x=2,y=2,z=2, the cube at
+x=0,y=2,z=3, and so on.
+
+During a cycle, all cubes simultaneously change their state according to the
+following rules:
+
+If a cube is active and exactly 2 or 3 of its neighbors are also active, the
+cube remains active. Otherwise, the cube becomes inactive.
+If a cube is inactive but exactly 3 of its neighbors are active, the cube
+becomes active. Otherwise, the cube remains inactive.
+The engineers responsible for this experimental energy source would like you
+to simulate the pocket dimension and determine what the configuration of cubes
+should be at the end of the six-cycle boot process.
+
+For example, consider the following initial state:
+
+.#.
+..#
+###
+Even though the pocket dimension is 3-dimensional, this initial state
+represents a small 2-dimensional slice of it. (In particular, this initial
+state defines a 3x3x1 region of the 3-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following
+configurations, where the result of each cycle is shown layer-by-layer at each
+given z coordinate (and the frame of view follows the active cells in each
+cycle):
+
+Before any cycles:
+
+z=0
+.#.
+..#
+###
+
+
+After 1 cycle:
+
+z=-1
+#..
+..#
+.#.
+
+z=0
+#.#
+.##
+.#.
+
+z=1
+#..
+..#
+.#.
+
+
+After 2 cycles:
+
+z=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1
+..#..
+.#..#
+....#
+.#...
+.....
+
+z=0
+##...
+##...
+#....
+....#
+.###.
+
+z=1
+..#..
+.#..#
+....#
+.#...
+.....
+
+z=2
+.....
+.....
+..#..
+.....
+.....
+
+
+After 3 cycles:
+
+z=-2
+.......
+.......
+..##...
+..###..
+.......
+.......
+.......
+
+z=-1
+..#....
+...#...
+#......
+.....##
+.#...#.
+..#.#..
+...#...
+
+z=0
+...#...
+.......
+#......
+.......
+.....##
+.##.#..
+...#...
+
+z=1
+..#....
+...#...
+#......
+.....##
+.#...#.
+..#.#..
+...#...
+
+z=2
+.......
+.......
+..##...
+..###..
+.......
+.......
+.......
+After the full six-cycle boot process completes, 112 cubes are left in the
+active state.
+
+Starting with your given initial configuration, simulate six cycles. How many
+cubes are left in the active state after the sixth cycle?
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+For some reason, your simulated results don't match what the experimental
+energy source engineers expected. Apparently, the pocket dimension actually has
+four spatial dimensions, not three.
+
+The pocket dimension contains an infinite 4-dimensional grid. At every integer
+4-dimensional coordinate (x,y,z,w), there exists a single cube (really, a
+hypercube) which is still either active or inactive.
+
+Each cube only ever considers its neighbors: any of the 80 other cubes where
+any of their coordinates differ by at most 1. For example, given the cube at
+x=1,y=2,z=3,w=4, its neighbors include the cube at x=2,y=2,z=3,w=3, the cube at
+x=0,y=2,z=3,w=4, and so on.
+
+The initial state of the pocket dimension still consists of a small flat region
+of cubes. Furthermore, the same rules for cycle updating still apply: during
+each cycle, consider the number of active neighbors of each cube.
+
+For example, consider the same initial state as in the example above. Even
+though the pocket dimension is 4-dimensional, this initial state represents a
+small 2-dimensional slice of it. (In particular, this initial state defines a
+3x3x1x1 region of the 4-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following
+configurations, where the result of each cycle is shown layer-by-layer at each
+given z and w coordinate:
+
+Before any cycles:
+
+z=0, w=0
+.#.
+..#
+###
+
+
+After 1 cycle:
+
+z=-1, w=-1
+#..
+..#
+.#.
+
+z=0, w=-1
+#..
+..#
+.#.
+
+z=1, w=-1
+#..
+..#
+.#.
+
+z=-1, w=0
+#..
+..#
+.#.
+
+z=0, w=0
+#.#
+.##
+.#.
+
+z=1, w=0
+#..
+..#
+.#.
+
+z=-1, w=1
+#..
+..#
+.#.
+
+z=0, w=1
+#..
+..#
+.#.
+
+z=1, w=1
+#..
+..#
+.#.
+
+
+After 2 cycles:
+
+z=-2, w=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1, w=-2
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=-2
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=1, w=-2
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-2, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=-1, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=-2, w=0
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=-1, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=0
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=-2, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=-1, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=-2, w=2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1, w=2
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=2
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=1, w=2
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=2
+.....
+.....
+..#..
+.....
+.....
+After the full six-cycle boot process completes, 848 cubes are left in the
+active state.
+
+Starting with your given initial configuration, simulate six cycles in a
+4-dimensional space. How many cubes are left in the active state after the
+sixth cycle?
+"""
+
+from aoc_helper import get_input
+import numpy as np
+
+
+def get_neighbours(grid, point):
+    def trim_neg(x): return 0 if x < 0 else x
+    neighbours = list(grid[trim_neg(point[0]-1):trim_neg(point[0]+2),
+                           trim_neg(point[1]-1):trim_neg(point[1]+2),
+                           trim_neg(point[2]-1):trim_neg(point[2]+2)].flat)
+    actives = neighbours.count('#')
+    if grid[point] == '#':
+        actives -= 1
+    return actives
+
+
+def compute_grid(grid, cycle):
+    grid_temp = grid.copy()
+    for layer in range(len(grid)):
+        for row in range(len(grid[0])):
+            for col in range(len(grid[0, 0])):
+                stats = get_neighbours(grid, (layer, row, col))
+                if grid[layer, row, col] == '#' and not (stats in [2, 3]):
+                    grid_temp[layer, row, col] = '.'
+                elif grid[layer, row, col] == '.' and stats == 3:
+                    grid_temp[layer, row, col] = '#'
+    if cycle > 1:
+        return compute_grid(grid_temp, cycle - 1)
+    elif cycle == 1:
+        return grid_temp
+
+
+data = get_input()
+
+input_matrix = np.array([list(i) for i in data])
+input_matrix = np.pad(input_matrix, (16, 16),
+                      mode='constant', constant_values='.')
+
+target_matrix = np.full((40, 40, 40), '.', dtype=np.str)
+target_matrix[18] = input_matrix
+
+counter_a = 0
+for i in compute_grid(target_matrix, 6):
+    for j in i:
+        for k in j:
+            if k == '#':
+                counter_a += 1
+
+print(f'Result: {counter_a}')
